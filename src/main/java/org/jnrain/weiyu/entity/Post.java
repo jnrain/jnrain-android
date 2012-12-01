ef095952ba395db0204ad9c4a0094f5393b298e4
Wang Xuerui <idontknw.wang@gmail.com>
2012-12-02 01:19:14 +0800

    update Post structure

--- src/main/java/org/jnrain/weiyu/entity/Post.java
+++ src/main/java/org/jnrain/weiyu/entity/Post.java
@@ -15,6 +15,8 @@
  */
 package org.jnrain.weiyu.entity;
 
+import java.util.Date;
+
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -23,8 +25,10 @@ public class Post {
 	private String _title;
 	private String _author;
 	private String _board;
+	private Date _ctime;
 	private int _replies;
 	private String _content;
+	private String _sign;
 
 	public long getID() {
 		return this._id;
@@ -58,6 +62,14 @@ public class Post {
 		this._board = board;
 	}
 
+	public Date getCtime() {
+		return this._ctime;
+	}
+
+	public void setCtime(long ctime) {
+		this._ctime = new Date(1000 * ctime);
+	}
+
 	public int getReplies() {
 		return this._replies;
 	}
@@ -72,6 +84,14 @@ public class Post {
 
 	public void setContent(String content) {
 		this._content = content;
+	}
+
+	public String getSign() {
+		return this._sign;
+	}
+
+	public void setSign(String sign) {
+		this._sign = sign;
 	}
 
 	public String toString() {
