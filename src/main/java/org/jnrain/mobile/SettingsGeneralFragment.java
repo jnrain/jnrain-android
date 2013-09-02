62b99b708508dfb929808f2d38d43b370199721d
Wang Xuerui <idontknw.wang@gmail.com>
2013-09-02 17:33:37 +0800

    允许配置更新频道

--- src/main/java/org/jnrain/mobile/SettingsGeneralFragment.java
+++ src/main/java/org/jnrain/mobile/SettingsGeneralFragment.java
@@ -15,10 +15,20 @@
  */
 package org.jnrain.mobile;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import org.jnrain.mobile.config.ConfigConstants;
+import org.jnrain.mobile.config.UpdaterConfigUtil;
+import org.jnrain.mobile.updater.UpdateChannel;
+import org.jnrain.mobile.updater.UpdateInfo;
+import org.jnrain.mobile.util.AppVersionHelper;
+import org.jnrain.mobile.util.GlobalState;
 import org.jnrain.mobile.util.PreferenceListFragment;
 import org.jnrain.mobile.util.preference.SeekBarPreference;
+import org.jnrain.mobile.util.preference.SummarizedListPreference;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -40,6 +50,9 @@ public class SettingsGeneralFragment extends PreferenceListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // dynamically add preference for update channel
+        addUpdateChannelPreference();
 
         exitBehavior = (ListPreference) findPreference(ConfigConstants.EXIT_BEHAVIOR);
         exitDoubleclickTimeout = (SeekBarPreference) findPreference(ConfigConstants.EXIT_DOUBLECLICK_TIMEOUT);
@@ -70,5 +83,60 @@ public class SettingsGeneralFragment extends PreferenceListFragment {
         } else {
             scr.removePreference(exitDoubleclickTimeout);
         }
+    }
+
+    public void addUpdateChannelPreference() {
+        Activity activity = getActivity();
+        UpdateInfo updInfo = GlobalState.getUpdateInfo();
+        SummarizedListPreference pref = new SummarizedListPreference(
+                activity);
+
+        String[] names;
+        String[] values;
+
+        if (updInfo == null) {
+            // never checked update, set to hardcoded values
+            names = new String[] {
+                    AppVersionHelper.getLocalizedNameForUpdateChannel(
+                            activity,
+                            "stable"),
+                    AppVersionHelper.getLocalizedNameForUpdateChannel(
+                            activity,
+                            "next")
+            };
+            values = new String[] {
+                    "stable",
+                    "next"
+            };
+        } else {
+            // generate channel list according to update info
+            HashMap<String, UpdateChannel> channels = updInfo.getChannels();
+            names = new String[channels.size()];
+            values = new String[channels.size()];
+
+            Iterator<String> iter = channels.keySet().iterator();
+            int idx = 0;
+            while (iter.hasNext()) {
+                String name = iter.next();
+                UpdateChannel chan = channels.get(name);
+
+                names[idx] = chan.getLocalizedName(activity);
+                values[idx] = name;
+
+                idx++;
+            }
+        }
+
+        // set up preference object
+        pref.setTitle(R.string.prefs_updater_channel);
+        pref.setDialogTitle(R.string.prefs_updater_channel_select);
+        pref.setKey(UpdaterConfigUtil.CURRENT_UPDATE_CHANNEL);
+        pref.setSummary("%s");
+
+        pref.setEntries(names);
+        pref.setEntryValues(values);
+        pref.setDefaultValue(UpdaterConfigUtil.DEFAULT_UPDATE_CHANNEL);
+
+        this.getPreferenceScreen().addPreference(pref);
     }
 }
