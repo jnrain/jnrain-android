9c226996be338def92d7b35218bfb77e21d2dae8
Wang Xuerui <idontknw.wang@gmail.com>
2013-11-01 21:54:50 +0800

    添加密码不能与用户名相同的检测

--- src/main/java/org/jnrain/mobile/accounts/kbs/KBSRegisterActivity.java
+++ src/main/java/org/jnrain/mobile/accounts/kbs/KBSRegisterActivity.java
@@ -337,6 +337,10 @@ public class KBSRegisterActivity extends JNRainActivity<KBSRegisterResult>
                         return;
                     }
 
+                    if (getUID().equalsIgnoreCase(s.toString())) {
+                        updateValidation(editNewPassword, false, true, 0);
+                    }
+
                     updateValidation(
                             editNewPassword,
                             true,
