08c34a82fdb26a1eab950585c2e546b55f12a481
Wang Xuerui <idontknw.wang@gmail.com>
2013-11-01 21:54:24 +0800

    实现注册返回结果提示

--- src/main/java/org/jnrain/mobile/accounts/kbs/KBSRegisterRequestListener.java
+++ src/main/java/org/jnrain/mobile/accounts/kbs/KBSRegisterRequestListener.java
@@ -84,8 +84,53 @@ public class KBSRegisterRequestListener
 
         switch (status) {
             case 0:
-                // success, send back result
+                // success, toast and send back result
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_0);
                 _ui.onRegisterSuccess(_uid, _psw);
+                break;
+
+            // Various failures
+            case 101:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_101);
+                break;
+            case 201:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_201);
+                break;
+            case 202:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_202);
+                break;
+            case 203:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_203);
+                break;
+            case 204:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_204);
+                break;
+            case 299:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_299);
+                break;
+            case 301:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_301);
+                break;
+            case 302:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_302);
+                break;
+            case 303:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_303);
+                break;
+            case 304:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_304);
+                break;
+            case 305:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_305);
+                break;
+            case 306:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_306);
+                break;
+            case 307:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_307);
+                break;
+            case 399:
+                ToastHelper.makeTextToast(ctx, R.string.reg_status_399);
                 break;
 
             default:
